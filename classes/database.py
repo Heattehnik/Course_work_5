05c349e852d413a35d13d5b84c8117e3b6dbdf55
Heattehnik <gadjka@mail.ru>
2023-05-26 14:17:54 +0500

    Add in classes package api.py, add HHVacancy class in database.py

--- classes/database.py
+++ classes/database.py
@@ -11,10 +11,61 @@ class DatabaseConnector:
     __db_name = os.getenv('DATABASE_NAME')
     __db_user = os.getenv('DATABASE_USER')
     __db_password = os.getenv('DATABASE_PASSWORD')
+    connect = psycopg2.connect(host=__db_address,
+                               database=__db_name,
+                               user=__db_user,
+                               password=__db_password)
+    cursor = connect.cursor()
 
-    def __init__(self):
-        self.connect = psycopg2.connect(host=self.__db_address,
-                                        database=self.__db_name,
-                                        user=self.__db_user,
-                                        password=self.__db_password)
-        self.cursor = self.connect.cursor()
+
+class HHVacancy(DatabaseConnector):
+    """
+    Класс для обработки вакансий полученных с hh.ru
+    """
+    all_vacancies = []
+    salary_from = 0
+    salary_to = 0
+    currency = 'RUR'
+
+    def __init__(self, vacancy) -> None:
+        self.platform = 'HeadHunter'
+        self.vacancy_id = vacancy['id']
+        self.title = vacancy['name']
+        self.requirement = vacancy['snippet']['requirement']
+        self.responsibility = vacancy['snippet']['responsibility']
+        self.employer = vacancy['employer']['name']
+        if vacancy.get('salary'):
+            if vacancy.get('salary').get('from'):
+                self.salary_from = vacancy.get('salary').get('from')
+            if vacancy.get('salary').get('to'):
+                self.salary_to = vacancy['salary']['to']
+            self.currency = vacancy['salary']['currency']
+        self.url = vacancy['alternate_url']
+        self.all_vacancies.append(self)
+
+    def __str__(self) -> str:
+        return f'{self.title}\n{self.salary_from}\n{self.salary_to}\n{self.currency}'
+
+    @classmethod
+    def insert_data(cls) -> None:
+        """
+        Метод класса для записи полученных вакансий в базу данных
+        """
+        for vacancy in cls.all_vacancies:
+            cls.cursor.execute(f"SELECT * FROM vacancies WHERE title = '{vacancy.title}' AND "
+                               f"requirement = '{vacancy.requirement}'")
+            result = cls.cursor.fetchone()
+            if not result:
+                cls.cursor.execute('INSERT INTO vacancies (platform, vacancy_id, title, requirement, responsibility,'
+                                   'employer, salary_from, salary_to, currency, url) VALUES (?,?,?,?,?,?,?,?,?,?)',
+                                   (vacancy.platform, vacancy.vacancy_id, vacancy.title, vacancy.requirement,
+                                    vacancy.responsibility, vacancy.employer, vacancy.salary_from, vacancy.salary_to,
+                                    vacancy.currency, vacancy.url))
+                cls.connect.commit()
+
+    @classmethod
+    def delete_from_db(cls) -> None:
+        """Удаление результата работы метода insert_data"""
+        for vacancy in cls.all_vacancies:
+            cls.cursor.execute(f"DELETE FROM vacancies WHERE vacancy_id = '{vacancy.vacancy_id}'")
+            cls.connect.commit()
